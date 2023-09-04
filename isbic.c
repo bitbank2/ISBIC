ec6a8855829e6b4113616c8044029ad8aab52d02
Laurence Bank <laurencebank@Laurences-Air.home>
2023-09-04 14:22:59 +0100

    added a short chunk and compression is now a little better

--- isbic.c
+++ isbic.c
@@ -32,27 +32,22 @@ uint8_t uc, *s;
 //
 static int isbicGetRun(ISBICIMG *pIMG)
 {
-int iRun = 0;
-int x, y; // keep original x/y in case we get a short run
-uint8_t ucColor;
-
-        x = pIMG->x; y = pIMG->y; // keep starting point
-	pIMG->ucColor = ucColor = isbicRead1Pixel(pIMG); // starting color to match
-        while (iRun < MAX_RUN && pIMG->ucColor == ucColor && pIMG->y < pIMG->iHeight) {
-		iRun++;
-		ucColor = isbicRead1Pixel(pIMG);
-        }
-        if (iRun < 8) { // no compression, reset x,y to start
-		pIMG->x = x; pIMG->y = y;
-        } else { // a good run, but we still need to back up 1 pixel
-		pIMG->x--;
-                if (pIMG->x < 0) {
-			pIMG->x = pIMG->iWidth-1;
-			if (pIMG->y != pIMG->iHeight) // don't decrement the last pixel or it will loop forever
-				pIMG->y--;
-		}
-        }
-return iRun;
+    int iRun = 0;
+    uint8_t ucColor;
+    
+    pIMG->ucColor = ucColor = isbicRead1Pixel(pIMG); // starting color to match
+    while (iRun < MAX_RUN && pIMG->ucColor == ucColor && pIMG->y < pIMG->iHeight) {
+        iRun++;
+        ucColor = isbicRead1Pixel(pIMG);
+    }
+    // back up over the last pixel since it's a different color
+    pIMG->x--;
+    if (pIMG->x < 0) {
+        pIMG->x = pIMG->iWidth-1;
+        if (pIMG->y != pIMG->iHeight) // don't decrement the last pixel or it will loop forever
+            pIMG->y--;
+    }
+    return iRun;
 } /* isbicGetRun() */
 
 //
@@ -61,40 +56,52 @@ return iRun;
 //
 int isbicCompress(uint8_t *pDest, ISBICIMG *pIMG)
 {
-int iOutSize = 0;
-int iRun;
-uint8_t *d, uc;
+int x1, y1, iOutSize = 0;
+int iRun, iRun2;
+uint8_t *d, uc, ucColor;
 
 	d = pDest;
 	d[0] = HEADER_BYTE_0;
-	d[1] = HEADER_BYTE_1;
+	d[1] = HEADER_BYTE_1 | (pIMG->pImage[0] >> 7); // store first color in header
 	d[2] = (uint8_t)pIMG->iWidth;
 	d[3] = (uint8_t)(pIMG->iWidth >> 8);
 	d[4] = (uint8_t)pIMG->iHeight;
 	d[5] = (uint8_t)(pIMG->iHeight >> 8);
 	d += 6;
-        pIMG->x = pIMG->y = 0;
-	while (pIMG->x < pIMG->iWidth && pIMG->y < pIMG->iHeight) { // encode loop
-		iRun = isbicGetRun(pIMG);  
-                if (iRun < 8) { // no compression, encode the next 7 pixels
-                        uc = 0;
-                        for (int i=6; i>=0; i--) {
-                            uc |= (isbicRead1Pixel(pIMG) << i);
-			}
-                	*d++ = (0x80 | uc);
-                } else { // encode the run
-                	uc = (pIMG->ucColor << 6); // high bit is color
-                        uc |= (iRun - 8);
-                        *d++ = uc;
+    pIMG->x = pIMG->y = 0;
+    while (pIMG->x < pIMG->iWidth && pIMG->y < pIMG->iHeight) { // encode loop
+        x1 = pIMG->x; y1 = pIMG->y; // preserve start of all runs
+        iRun = isbicGetRun(pIMG);
+        ucColor = pIMG->ucColor; // color of this run
+        if (iRun > 8) { // long run
+            uc = (ucColor << 6); // high bit is color
+            uc |= (iRun - 8);
+            *d++ = uc | CHUNK_LONG;
+        } else {
+            iRun2 = isbicGetRun(pIMG);
+            if (iRun + iRun2 < 7) { // not worth using RLE1/2
+                pIMG->x = x1; pIMG->y = y1; // restore to start
+                uc = 0;
+                for (int i=5; i>=0; i--) { // store 6 pixels
+                    uc |= (isbicRead1Pixel(pIMG) << i);
+                } // for i
+                *d++ = (CHUNK_UNCOMP | uc);
+            } else { // we can use the short R1/R2 encoding
+                if (iRun2 > 4) { // need to encode the next part in a different chunk
+                    pIMG->x -= (iRun2 - 4); // excess run length
+                    while (pIMG->x < 0) {
+                        pIMG->x += pIMG->iWidth;
+                        pIMG->y--;
+                    }
+                    iRun2 = 4;
                 }
-	} // while encoding
+                *d++ = CHUNK_SHORT | (ucColor << 5) | ((iRun-1) << 2) | (iRun2-1);
+            }
+        } // short/uncomp chunk
+    } // while encoding
 	iOutSize = (int)(d - pDest);
 return iOutSize;
 } /* isbicCompress() */
-
-void isbicDecompress(uint8_t *pSrc, int iLen, uint8_t *pDest, int iWidth, int iHeight, int iPitch)
-{
-} /* isbicDecompress() */
 
 //
 // Initialize the decode process
@@ -106,18 +113,29 @@ uint8_t uc, *s;
 
 	s = pSrc;
 	if (!s) return 0;
-	if (s[0] != HEADER_BYTE_0 || s[1] != HEADER_BYTE_1) return 0; // not an ISBIC file
+	if (s[0] != HEADER_BYTE_0 || (s[1] & 0xfe) != HEADER_BYTE_1) return 0; // not an ISBIC file
 	pIMG->iWidth = (s[2] | ((uint16_t)s[3] << 8));
 	pIMG->iHeight = (s[4] | ((uint16_t)s[5] << 8));
-	uc = s[6]; // first compressed byte
-	pIMG->ucColor = uc; // keep the current data byte here for decoding
+	pIMG->ucChunk = uc = s[6]; // first compressed byte
+	pIMG->ucColor = s[1] & 1; // first color is saved in the header byte
 	pIMG->pImage = &s[7]; // next 1 byte code
 	pIMG->x = pIMG->y = 0; // destination x,y
 	// Set initial repeat/non-repeat count
-	if (uc & 0x80) // non-repeating
-		pIMG->len = 7;
-	else
-		pIMG->len = (uc & 0x3f) + 8;
+    switch (uc & CHUNK_MASK) {
+        case CHUNK_UNCOMP: // non-repeating
+            pIMG->len = 6;
+            pIMG->len2 = 0;
+            break;
+        case CHUNK_SHORT:
+            pIMG->len = ((uc >> 2) & 7)+1;
+            pIMG->len2 = (uc & 3) + 1;
+            break;
+        case CHUNK_LONG: // long run length
+        case CHUNK_LONG2:
+            pIMG->len = (uc & 0x3f) + 8;
+            pIMG->len2 = 0;
+            break;
+    }
 	return 1;
 } /* isbicDecodeInit() */
 
@@ -131,19 +149,48 @@ uint8_t uc;
 
 	if (!pIMG) return 0;
 	if (pIMG->y >= pIMG->iHeight) return 0; // past bottom
-	if (pIMG->len == 0) { // get the next data chunk
-		pIMG->ucColor = *pIMG->pImage++;
-		if (pIMG->ucColor & 0x80) // uncompressed
-			pIMG->len = 7;
-		else
-			pIMG->len = 8 + (pIMG->ucColor & 0x3f);
-	}
-	if (pIMG->ucColor & 0x80) { // last data chunk was uncompressed
-		uc = (pIMG->ucColor >> (pIMG->len-1)) & 1; 
-	} else { // compressed has the color in bit 6
-		uc = (pIMG->ucColor >> 6);
-	}
-	pIMG->len--;
+    if (pIMG->len == 0 && pIMG->len2 == 0) { // get the next data chunk
+        uc = pIMG->ucChunk = *pIMG->pImage++;
+        switch (uc & CHUNK_MASK) {
+            case CHUNK_UNCOMP: // uncompressed
+                pIMG->len = 6;
+                break;
+            case CHUNK_SHORT:
+                pIMG->ucColor = (uc >> 5) & 1;
+                pIMG->len = ((uc >> 2) & 7)+1;
+                pIMG->len2 = (uc & 3) + 1;
+                break;
+            case CHUNK_LONG:
+            case CHUNK_LONG2:
+                pIMG->ucColor = (uc >> 6) & 1;
+                pIMG->len = 8 + (uc & 0x3f);
+                break;
+        } // switch
+    } // need to read the next compressed data chunk
+    
+    // Pull 1 pixel from the current chunk
+	switch (pIMG->ucChunk & CHUNK_MASK) {
+        case CHUNK_UNCOMP:
+            uc = (pIMG->ucChunk >> (pIMG->len-1)) & 1;
+            pIMG->len--;
+            break;
+            
+        case CHUNK_SHORT:
+            if (pIMG->len) {
+                uc = pIMG->ucColor;
+                pIMG->len--;
+            } else { // second half is opposite color
+                uc = 1-pIMG->ucColor;
+                pIMG->len2--;
+            }
+            break;
+            
+        case CHUNK_LONG:
+        case CHUNK_LONG2:
+            uc = pIMG->ucColor;
+            pIMG->len--;
+            break;
+	} // switch on chunk type
 	// increment the current pixel
 	pIMG->x++;
 	if (pIMG->x >= pIMG->iWidth) {
--- isbic.h
+++ isbic.h
@@ -17,7 +17,7 @@
 // 2 byte height in little endian order
 //
 #define HEADER_BYTE_0 0xfe
-#define HEADER_BYTE_1 0xed
+#define HEADER_BYTE_1 0xec
 //
 // each byte of the file is an atomic 'code' that independently encodes
 // 8-71 compressed (same color) pixels or 7 uncompressed pixels
@@ -25,18 +25,24 @@
 // the maximum length of run we can store in a single byte of output
 #define MAX_RUN 71
 //
-// 2 basic encoding blocks:
+// 3 basic encoding blocks:
 // A: 0CLLLLLL (C = color, L = run length (8-71)
-// B: 1PPPPPPP (P = 7 non-repeating pixels)
-//
+// B: 10PPPPPP (P = 6 non-repeating pixels)
+// C: 11CRRRrr (RRR = run 1-8, rr = opposite color, run 1-4)
+#define CHUNK_MASK 0xc0
+#define CHUNK_LONG 0x00
+#define CHUNK_LONG2 0x40
+#define CHUNK_SHORT 0xc0
+#define CHUNK_UNCOMP 0x80
 
 typedef struct tagisbicimage
 {
   uint8_t *pImage; // source or destination image
   int iWidth, iHeight, iPitch; // image dimensions and bytes per line
   int x, y; // current encoder or decoder position
-  int len; // remaining length of current run being decoded
+  int len, len2; // remaining length of current run(s) being decoded
   uint8_t ucColor; // current color
+  uint8_t ucChunk; // current chunk being decoded
 } ISBICIMG;
 //
 // Public functions
